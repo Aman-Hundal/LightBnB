389683ca0f36ed75297e0bc5172f8e472f17961b
Aman Hundal <64342261+KewlKewlKewl@users.noreply.github.com>
2021-10-15 03:28:45 +0000

    Added property search functionality

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,3 +1,4 @@
+const { query } = require('express');
 const { Pool } = require('pg');
 const pool = new Pool ({
   user: 'vagrant',
@@ -128,9 +129,6 @@ const getAllReservations = function(guest_id, limit = 10) {
   .catch((err) => {
     console.error(err.message);
   });
-
-
-  // return getAllProperties(null, 2);
 };
 exports.getAllReservations = getAllReservations;
 
@@ -144,22 +142,47 @@ exports.getAllReservations = getAllReservations;
  */
 
 const getAllProperties = function(options, limit = 10) { // PROMISE OBJECT IS NEEDED HERE BECUASE When getAllProperties is called in the routes file, it is chained to .then, which can only consume a promise.
-  const queryString = 
-  `SELECT *
-  FROM properties
-  LIMIT $1`;
-  const values = [limit];
+    const queryParams = [];
+    let queryString = `
+    SELECT properties.*, avg(property_reviews.rating) as average_rating
+    FROM properties
+    JOIN property_reviews ON properties.id = property_id
+    WHERE 1 = 1
+    `;
+    
+    if (options.city) {
+      queryParams.push(`%${options.city}%`);
+      queryString += `AND city LIKE $${queryParams.length} `;
+    }
+    
+    if (options.owner_id) {
+      queryParams.push(options.owner_id);
+      queryString += `AND owner_id = $${queryParams.length} `;
+    }
+
+    if (options.minimum_price_per_night && options.maximum_price_per_night) {
+      queryParams.push(options.minimum_price_per_night * 100);
+      queryParams.push(options.maximum_price_per_night * 100);
+      queryString += `AND cost_per_night BETWEEN $${queryParams.length-1} AND $${queryParams.length} `;
+    }
+    
+    queryString += `GROUP BY properties.id ` 
+    
+    if (options.minimum_rating) {
+      queryParams.push(options.minimum_rating);
+      queryString += `HAVING avg(rating) >= $${queryParams.length} `;
+    }
+
+    queryParams.push(limit);
+    queryString += `
+    ORDER BY cost_per_night
+    LIMIT $${queryParams.length};
+    `;
   
-  return pool // you need a return here so that this entire pool promise object is returned as a promise object to the .then /properties in the api routes. The /properties calls this function and cahins to a .then WHICH ONLY CAN TAKE A PROMISE OBJECT. So this entire promise pool object must be returned so the function call which chains to a .then in /properties can be executed.
-  .query(queryString, values) 
-  .then((res) => {
-    // console.log(res.rows);
-    return res.rows; // THIS RETURN IS NEEDED SO THAT THE PROMISE OBJECT (RETURN POOL) CARRIES WITH IT THE RESULT OF THIS QUERY PROMISE. THIS REUTRN RES.ROWS IS PIGGIED BACK AS A RESULT WITH THE RESULT POOL AND THE RESULT QUERY DATA CAN BE PASED TO /PROPERTIES .THEN CHAIN. 
-  })
-  .catch((e) => {
-    // console.log(e.message);
-    return e.message;
-  });
+    console.log(queryString, queryParams);
+
+    return pool.query(queryString, queryParams).then((res) => res.rows);
+  
 };
 exports.getAllProperties = getAllProperties;
 
